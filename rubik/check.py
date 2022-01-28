f712bc40c3939a9ad5455fc587324efca8b52376
Caleb Fahlgren <calebfahlgren@protonmail.com>
2022-01-28 15:33:09 -0600

    add unit tests and build check method

--- rubik/check.py
+++ rubik/check.py
@@ -1,10 +1,37 @@
+from email.encoders import encode_7or8bit
+from enum import unique
 import rubik.cube as rubik
+from collections import defaultdict
 
 def _check(parms):
     result={}
-    encodedCube = parms.get('cube',None)       
+    encodedCube = parms.get('cube',None)     
+
+    def has_correct_colors(cube: str):
+        unique_color_count = defaultdict(int)
+
+        for color in cube:
+            unique_color_count[color] += 1
+
+        # check if there are 6 unique colors
+        if (len(unique_color_count) != 6):
+            return False
+        
+        # check for 9 occurences of each color
+        for color, occur in unique_color_count.items():
+            if occur != 9:
+                return False
+        
+        return True
+        
     if(encodedCube == None):
-        result['status'] = 'error: xxx'
+        result['status'] = 'error: no value present'
+    elif(not isinstance(encodedCube, str)):
+        result['status'] = 'error: value is of wrong type'
+    elif(len(encodedCube) != 54):
+        result['status'] = 'error: incorrect character count'
+    elif(not has_correct_colors(encodedCube)):
+        result['status'] = 'error: incorrect character count'
     else:
         result['status'] = 'ok'
     return result
--- rubik/test/checkTest.py
+++ rubik/test/checkTest.py
@@ -11,3 +11,87 @@ class CheckTest(TestCase):
         status = result.get('status', None)
         self.assertEqual(status, 'ok')
 
+    """
+    Test that cube value is present
+    """
+    def test_check_020_ShouldReturnErrorNoValuePresent(self):
+        parm = {'op':'check'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: no value present')
+
+    """
+    Test that cube value is of type str
+    """
+    def test_check_030_ShouldReturnErrorIncorrectValue(self):
+        parm = {'op':'check', 'cube': 2}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: value is of wrong type')
+    
+    """
+    Test cube character size at boundary
+    """
+    def test_check_040_ShouldReturnOkOnSolvedCube(self):
+        parm = {'op':'check', 'cube': 'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'ok')
+    
+    """
+    Test cube character size above boundary
+    """
+    def test_check_050_ShouldErrorIncorrectCharCount(self):
+        parm = {'op':'check', 'cube': 'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwwwbbbbbbbbb'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: incorrect character count')
+    
+    """
+    Test cube character size below boundary
+    """
+    def test_check_060_ShouldReturnErrorIncorrectCharCount(self):
+        parm = {'op':'check', 'cube': 'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyy'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: incorrect character count')
+
+    """
+    Test cube character groups and occurences
+    """
+    def test_check_070_ShouldReturnOkOnSolvedCube(self):
+        parm = {'op':'check', 'cube': 'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'ok')
+    
+    """
+    Test incorrect cube color occurences
+    """
+    def test_check_090_ShouldErrorIncorrectCharCount(self):
+        parm = {'op':'check', 'cube': 'bbbbbbbrrrrrrrrrrrgggggggggoooooooooyyyyyyyyy'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: incorrect character count')
+    
+    """
+    Test incorrect cube unique colors 
+    """
+    def test_check_100_ShouldErrorIncorrectCharCount(self):
+        parm = {'op':'check', 'cube': 'ffffffaaaaaaeeeeeerrrrrrttttttyyyyyyuuuuuuiiiiiillllll'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: incorrect character count')
+
+
+
+
+
